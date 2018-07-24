85ca21d5f876b5978127b78105fec0d4285fb0f8
fengjinlong <18618166564@163.com>
2018-07-24 11:28:17 +0800

    adsa

---
+++ gen/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+    this.asyncReadFile()
+  },
+  methods: {
+    async asyncReadFile () {
+      console.log(f1.toString())
+      console.log(f2.toString())
+      const f2 = await this.fun2()
+      const f1 = await this.fun1()
+    },
+    fun1 () {
+      console.log('fun1+++++')
+      return 'fun1'
+    },
+    fun2 () {
+      console.log('fun2+++++')
+      return 'fun2'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
