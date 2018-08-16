e7c91321f329ee608516c45dc061ed2a4a584b9c
fengjinlong <18618166564@163.com>
2018-08-16 15:50:36 +0800

    asd

---
+++ hellorld/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+   HelloWorld
+   <H :mm="mm"></H>
+  </div>
+</template>
+
+<script>
+import H from 'hellovuefj'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      mm: 'adsafdasf33333333'
+    }
+  },
+  components: {
+    H
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
