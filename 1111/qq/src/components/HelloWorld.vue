7e527fad3789260022b45d87e81326930ab2f853
fengjinlong <18618166564@163.com>
2018-09-12 18:32:55 +0800

    d

---
+++ 1111/qq/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">{{myArray}}
+    <draggable v-model="myArray">
+        <transition-group>
+            <div class="div" v-for="element in myArray" :key="element.id">
+                {{element.name}}
+                <draggable v-model="element.my">
+                  <p v-for="(e,i) in element.my" :key="i">{{e}}</p>
+                </draggable>
+            </div>
+        </transition-group>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      myArray: [
+        {id: 1, name: 'qqqqq', my: [1, 2, 3, 4, 5]},
+        {id: 14, name: 'wwwwww', my: [1, 2, 3, 4, 5]},
+        {id: 12, name: 'eeeeee', my: [1, 2, 3, 4, 5]},
+        {id: 13, name: 'rrrrrr', my: [1, 2, 3, 4, 5]}
+      ]
+    }
+  },
+  components: {
+    draggable
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .div {
+    width: 100px;
+    border: 1px solid red;
+    margin-bottom: 10px;
+    cursor: pointer;
+  }
+  p {
+    padding: 10px;
+    border: 1px solid orange;
+  }
+</style>
