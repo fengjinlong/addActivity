6e1d8200fca9b1b935891d0982b3aa9236e2a364
fjl <18618166564@.163.com>
2018-12-09 23:01:08 +0800

    ad

--- html/5.js
+++ html/5.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-function once (fn) {
-  let a = false
-  return function () {
-    if (!a) {
-      a = true
-      console.log(this)
-      fn.apply(this, arguments)
-      // fn.apply(null,arguments)  均可 
-    }
-  }
-}
-
-function a () {
-  console.log(1)
-}
-
-let fun = once(a)
-fun()
-fun()
-fun()
-
-=======
 // 单例模式
 // let Ob = function () {
 //   console.log(1)
@@ -88,4 +65,3 @@ let f = function () {
   console.log(Array.prototype.shift.apply(arguments))
 }
 f(1,2,3,4,5)
->>>>>>> 03a3c5b15f511d280c04a7de9612877b97bc465a
